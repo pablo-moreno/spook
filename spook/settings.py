fe657462ab8b19638597ce3c40aa662fe892a26f
Pablo Moreno <pablomoreno.inf@gmail.com>
2020-09-01 21:45:06 +0200

    Update settings.py

--- spook/settings.py
+++ spook/settings.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
-EXTERNAL_API_URL = getattr('SPOOK_API_URL', settings, '')
-AUTHORIZATION_HEADER_NAME = getattr('SPOOK_AUTHORIZATION_HEADER_NAME', settings, 'Authorization')
-AUTHORIZATION_HEADER = getattr('SPOOK_AUTHORIZATION_HEADER', settings, 'Bearer')
+EXTERNAL_API_URL = getattr(settings, 'SPOOK_API_URL', '')
+AUTHORIZATION_HEADER_NAME = getattr(settings, 'SPOOK_AUTHORIZATION_HEADER_NAME', 'Authorization')
+AUTHORIZATION_HEADER = getattr(settings, 'SPOOK_AUTHORIZATION_HEADER', 'Bearer')
