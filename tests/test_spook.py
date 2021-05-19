5bbb166da73c03d8d2b655cb4cbdf2939ca33bd3
Pablo Moreno <pablo@kasfactory.net>
2021-05-19 12:16:39 +0200

    Remove repeated tests

--- tests/test_spook.py
+++ tests/test_spook.py
@@ -90,35 +90,3 @@ class ListCreateProductResourceView(APIResourceListCreateView):
 
     def get_token(self, request):
         return ''
-
-
-@patch('spook.resources.requests.get', get_mocked_products)
-def test_list_view_products(self):
-    view = ListCreateProductResourceView()
-    response = view.list(MockedRequest())
-    assert response.status_code == 200
-    assert response.data == PRODUCTS
-
-
-@patch('spook.resources.requests.get', retrieve_product)
-def test_retrieve_view_products(self):
-    view = APIResourceRetrieveUpdateDestroyView()
-    response = view.get(MockedRequest(), pk=1)
-    assert response.status_code == 200
-    assert response.data == PRODUCTS['results'][0]
-
-
-@patch('spook.resources.requests.post', create_product)
-def test_create_view_product(self):
-    view = ListCreateProductResourceView()
-    response = view.create(MockedRequest(data={'name': 'The Elder Scrolls V'}))
-    assert response.status_code == 201
-    assert response.data == CREATED_PRODUCT
-
-
-@patch('spook.resources.requests.put', update_product)
-def test_update_view_product(self):
-    view = APIResourceRetrieveUpdateDestroyView()
-    response = view.update(MockedRequest(data={'name': 'The Elder Scrolls V: Skyrim'}), pk=3)
-    assert response.status_code == 201
-    assert response.data == CREATED_PRODUCT
